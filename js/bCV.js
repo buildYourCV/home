f91a0d8c417484f210a2b40ff2a9bacab588113d
buildYourCV <abhishek3feb1991@gmail.com>
2025-08-24 01:51:11 +0530

    Add files via upload

--- js/bCV.js
+++ js/bCV.js
@@ -1,5 +1,46 @@
-function App() {
-    // alert("This alert is from build you CV!");
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Your JavaScript code here, or call a function
+    (function App() {
+        let bcvElement = document.getElementById('bCVMenuLink')
+        bcvElement.addEventListener('click', handleBCVOnClick);
+        let aboutElement = document.getElementById('aboutMenuLink');
+        aboutElement.addEventListener('click', handleAboutOnClick);
+    })();
+});
+
+const handleHomeOnClick= () => {
+    showHidePage('homeDiv');
 }
 
-App();
+const handleBCVOnClick = (e) => {
+    showHidePage('bcvDiv', e);
+}
+
+const handleGMCVOnClick = () => {
+    showHidePage('gmcvDiv')
+}
+
+const handleAboutOnClick = (e)=>  {
+    showHidePage('aboutDiv', e);
+}
+
+const showHidePage = (thisPageId, e) => {
+    try {
+        let thisPage = document.getElementById(thisPageId)
+        thisPage.style.display = 'block';
+        // e && e.target && e.target.classList.add('active');
+        let allPagesList = getPagesList();
+        allPagesList.map((item) => {
+            if (item !== thisPageId) {
+                document.getElementById(item).style.display = 'none';
+            }
+        });
+    } catch (err) {
+        console.error('Exception error', err);
+    }
+}
+
+const getPagesList = () => {
+    return ['homeDiv', 'bcvDiv', 'gmcvDiv', 'aboutDiv'];
+}
