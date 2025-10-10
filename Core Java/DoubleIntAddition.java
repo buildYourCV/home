67dd24551c75daff44df5494faea6b337043c74a
buildYourCV <abhishek3feb1991@gmail.com>
2025-10-11 02:05:29 +0530

    Delete Core Java directory

--- Core Java/DoubleIntAddition.java
+++
@@ -1,7 +0,0 @@
-public class DoubleIntAddition {
-    public static void main(String[] args){
-        int a= 7; double d= 8.99;
-
-        System.out.println(((Object)(a + d)).getClass().getName());
-    }
-}
